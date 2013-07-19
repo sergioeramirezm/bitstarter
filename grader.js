6526593f04b1a0447671de831c4644cd673443da
Sergio Ramirez <seramirezm@gmail.com>
2013-07-19 03:48:49 +0000

    Part 3 of Homework modification (read html file from URL)

--- grader.js
+++ grader.js
@@ -1,68 +1,100 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attribut
-es.
-Uses commander.js and cheerio. Teaches command line application developmen
-t
+
+Author: Sergio Enrique Ramirez Moreno
+Date: Jul 13 2013
+Description:
+Automatically grade files for the presence of specified HTML tags/attributes
+from a file or a URL.
+Uses commander.js and cheerio. Teaches command line application development
 and basic DOM parsing.
+
 References:
-+ cheerio
-- https://github.com/MatthewMueller/cheerio
-- http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-- http://maxogden.com/scraping-with-node.html
-+ commander.js
-- https://github.com/visionmedia/commander.js
-- http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-
-line-interfaces-made-easy
-+ JSON
-- http://en.wikipedia.org/wiki/JSON
-- https://developer.mozilla.org/en-US/docs/JSON
-- https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+
+var createFile = function (url){
+	var pageName ="test_page.html";
+	if(!fs.existsSync(pageName)) {
+        console.log("%s does not exist. A blank file is created but you need to run the program again", pageName);
+        fs.writeFileSync(pageName," ");
+    }
+	rest.get(url).on('complete', function(response){
+	fs.writeFileSync(pageName, response);});
+	return pageName;
+};
+
 var assertFileExists = function(infile) {
-	var instr = infile.toString();
-	if(!fs.existsSync(instr)) {
-		console.log("%s does not exist. Exiting.", instr);
-		process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-	}
-return instr;
+    var instr = infile.toString();
+    if(!fs.existsSync(instr)) {
+        console.log("%s does not exist. Exiting.", instr);
+        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
 };
+
 var cheerioHtmlFile = function(htmlfile) {
-	return cheerio.load(fs.readFileSync(htmlfile));
+    return cheerio.load(fs.readFileSync(htmlfile));
 };
+
 var loadChecks = function(checksfile) {
-	return JSON.parse(fs.readFileSync(checksfile));
+    return JSON.parse(fs.readFileSync(checksfile));
 };
+
 var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
-	var checks = loadChecks(checksfile).sort();
-	var out = {};
-	for(var ii in checks) {
-		var present = $(checks[ii]).length > 0;
-		out[checks[ii]] = present;
-	}
-	return out;
+    $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
 };
+
 var clone = function(fn) {
-	// Workaround for commander.js issue.
-	// http://stackoverflow.com/a/6772648
-	return fn.bind({});
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
 };
+
+
+
 if(require.main == module) {
-	program
-		.option('-c, --checks <check_file>', 'Path to checks.json', 
-	clone(assertFileExists), CHECKSFILE_DEFAULT)
-		.option('-f, --file <html_file>', 'Path to index.html',
-	clone(assertFileExists), HTMLFILE_DEFAULT)
-		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+    program
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url_path>', 'Path to web page',clone(createFile))
+        .parse(process.argv);
+	var hfile=null;
+	if (program.url==undefined)
+		hfile = program.file;
+	else
+		hfile=program.url;
+    var checkJson = checkHtmlFile(hfile, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    fs.writeFileSync("JsonOutput_sergio_ramirez.txt",outJson);
 } else {
-	exports.checkHtmlFile = checkHtmlFile;
+    exports.checkHtmlFile = checkHtmlFile;
 }
