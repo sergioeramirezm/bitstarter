882c4aa0d1c754a1acde3c2c151f0b077d4f6714
Sergio Ramirez <seramirezm@gmail.com>
2013-08-08 04:26:56 +0000

    Fixed bugs in row-fluid class and alt attributes.

--- web.js
+++ web.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var fs = require('fs');
-var infile = "index.html";
-var web_page=fs.readFileSync(infile, 'utf8');
+var htmlfile= "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(web_page);
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
